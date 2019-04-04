5d6f6b7267c0561a213dd78ffd5b0cbdf2e894e7
Edmilson Lopes Fernandes dos Santos <esantos@fitec.org.br>
2019-04-04 13:47:31 -0300

    Essa tabela será usado pela vivo.

---
+++ Tables/dbo.SITE_HANDLER_VIVO.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[SITE_HANDLER_VIVO]
+(
+[id] [int] NULL,
+[customer] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[vivo] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
